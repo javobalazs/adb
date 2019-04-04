a30e9fef13071a17ca6ca1aa709b0707eaa35e31
balazs <balazs@bacer.gnifanip.info>
2019-04-04 08:19:24 +0200

    Szinkronba hoztam az eredeti elixir_utils-zal

--- lib/util/timex_patch.ex
+++ lib/util/timex_patch.ex
@@ -1,5 +1,5 @@
 defmodule Util.TimexPatch do
-  @vsn "0.1.0"
+  @vsn "0.1.1"
   @moduledoc """
   Ami a `Timex`-ben problemas.
 
@@ -9,25 +9,26 @@ defmodule Util.TimexPatch do
   @doc """
   `:strftime` parse. `mask` is the mask.
 
-  Return: `{ts, x}`
+  Return: `{ts, at, x}`
   - `ts`: a timestamp, vagy egy jo kozelites. Ha teljesen remenytelen, akkor a helyi idot adja vissza, de mindig ertelmes idot.
+  - `at`: ha `:ok`, akkor valamilyen ertelemben jo ertek megy vissza, kulonben valami (rossz) tipp.
   - `x`: a hibauzenet, ha van.
   """
-  @spec parse_timestamp(String.t(), String.t()) :: {Integer.t(), String.t() | nil}
+  @spec parse_timestamp(String.t(), String.t()) :: {Integer.t(), :ok | :error, String.t() | nil}
   def parse_timestamp(timestamp, mask) do
     case Timex.parse(timestamp, mask, :strftime) do
       {:ok, x} ->
         y = x |> Timex.to_datetime(:local)
 
         case y do
-          %stru{} when stru in [Timex.AmbiguousDateTime] -> {Timex.to_unix(y.after), "Timex.to_datetime(#{timestamp}, :local) ambiguous: #{inspect(y)}."}
-          %stru{} when stru in [DateTime] -> {Timex.to_unix(y), nil}
-          {:error, msg} -> {System.system_time(:second), "Timex.to_datetime(#{timestamp}, :local) error: #{inspect(msg)}."}
-          msg -> {System.system_time(:second), "Timex.to_datetime(#{timestamp}, :local) unknown: #{inspect(msg)}."}
+          %stru{} when stru in [Timex.AmbiguousDateTime] -> {Timex.to_unix(y.after), :ok, "Timex.to_datetime(#{timestamp}, :local) ambiguous: #{inspect(y)}."}
+          %stru{} when stru in [DateTime] -> {Timex.to_unix(y), :ok, nil}
+          {:error, msg} -> {System.system_time(:second), :error, "Timex.to_datetime(#{timestamp}, :local) error: #{inspect(msg)}."}
+          msg -> {System.system_time(:second), :error, "Timex.to_datetime(#{timestamp}, :local) unknown: #{inspect(msg)}."}
         end
 
       {:error, msg} ->
-        {System.system_time(:second), "Timex.parse(#{timestamp}, ) error: #{inspect(msg)}."}
+        {System.system_time(:second), :error, "Timex.parse(#{timestamp}, #{mask}, :strftime) error: #{inspect(msg)}."}
     end
   end
 end
